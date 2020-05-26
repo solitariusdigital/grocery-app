d04a39cc95d64cc6c205a2bca47f3111a45c8722
Pouyan MAdanipour <pouyan.mad@gmail.com>
2020-05-26 20:17:56 +1000

    heroku port

--- server.js
+++ server.js
@@ -4,20 +4,19 @@ const productsList = require('./client/src/data/productsList');
 const app = express();
 
 app.get('/grocery', (req, res) => {
+	let searchTerm = req.query.searchTerm;
 
-    let searchTerm = req.query.searchTerm
-
-    if(!searchTerm) {
-        console.log('no search term returning the top 4 items, list of items:', productsList);
-        return res.json(productsList.slice(0, 4));
-    } else {
-        console.log('search term provided:', searchTerm);
-        const products = productsList.filter(product => {
-            return product.name.toLowerCase().match(searchTerm.toLowerCase())
-        });
-        return res.json(products);
-    }
+	if (!searchTerm) {
+		console.log('no search term returning the top 4 items, list of items:', productsList);
+		return res.json(productsList.slice(0, 4));
+	} else {
+		console.log('search term provided:', searchTerm);
+		const products = productsList.filter((product) => {
+			return product.name.toLowerCase().match(searchTerm.toLowerCase());
+		});
+		return res.json(products);
+	}
 });
 
-const port = 5000;
-app.listen(port, () => console.log(`server is listening on port ${port}`))
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server is listening on port ${port}`));
